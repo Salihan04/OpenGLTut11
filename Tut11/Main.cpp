12eb829c9461f0a260b42b90d0b77431e531bd18
Salihan04 <salihan04@gmail.com>
2015-02-18 19:14:37 +0800

    Included Freetype library

--- Tut11/Main.cpp
+++ Tut11/Main.cpp
@@ -10,6 +10,8 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <ft2build.h>
+#include <freetype/freetype.h>
 
 #include "Main.h"
 #include "Shader.h"
